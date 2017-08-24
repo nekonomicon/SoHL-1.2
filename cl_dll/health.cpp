53d4d59b9583c98a394c59e575450353fb1dc537
Night Owl <sepulkarium45@yahoo.com>
2017-08-24 12:17:58 +0500

    Do not draw separator.

--- cl_dll/health.cpp
+++ cl_dll/health.cpp
@@ -207,6 +207,7 @@ int CHudHealth::Draw(float flTime)
 	else
 		a = MIN_ALPHA;
 
+
 	// If health is getting low, make it bright red
 	if (m_iHealth <= 15)
 		a = 255;
@@ -228,16 +229,7 @@ int CHudHealth::Draw(float flTime)
 
 		x = CrossWidth + HealthWidth / 2;
 
-		x = gHUD.DrawHudNumber(x, y, DHN_3DIGITS | DHN_DRAWZERO, m_iHealth, r, g, b);
-
-		x += HealthWidth/2;
-
-		int iHeight = gHUD.m_iFontHeight;
-		int iWidth = HealthWidth/10;
-
-		UnpackRGB(r,g,b, gHUD.m_iHUDColor); //LRC
-		//LRC FillRGBA(x, y, iWidth, iHeight, 255, 160, 0, a);
-		FillRGBA(x, y, iWidth, iHeight, r, g, b, a); //LRC
+		gHUD.DrawHudNumber(x, y, DHN_3DIGITS | DHN_DRAWZERO, m_iHealth, r, g, b);
 	}
 
 	DrawDamage(flTime);
